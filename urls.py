f252ef6692e69e031f4ed95d532f8fb12d3102dd
praneeth-7606 <praneethvvsss@gmail.com>
2024-12-18 10:52:47 +0530

    removing comments and unnessary code

--- urls.py
+++ urls.py
@@ -1,10 +1,4 @@
-# from django.contrib import admin
-# from django.urls import path, include
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('ats/', include('ats.urls')),
-# ]
 from django.contrib import admin
 from django.urls import path, include
 from . import views
